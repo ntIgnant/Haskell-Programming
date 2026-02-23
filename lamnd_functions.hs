8fca2c101d9f9c3d798e40206cf80328167100e3
ignant <ignanttt@gmail.com>
2026-02-23 17:38:50 +0100

    UPDATED | basics of lamnda functions in haskell

---
+++ lamnd_functions.hs
@@ -0,0 +1,12 @@
+-- A "Lamnda" is for writing a function whitout giving it a name
+-- So a lamnda is a function, not a type itself
+-- Lamnda is denoted with '\' in haskell 
+
+-- Lamndas are specially usefull when there needs a small function only once, specially as an argument
+-- to another function.
+
+-- e.g with map
+map (\x -> x + 1) [1, 2, 3]
+-- This should result in: [2, 3, 4]
+
+
