f7107216034d45790852c7dfa512cf7cae6b83f9
ignant <ignanttt@gmail.com>
2026-03-10 21:24:50 +0100

    UPDATED | w5 ws

--- data_and_types.hs
+++ data_and_types.hs
@@ -12,6 +12,15 @@ data Person = Person String Int -- e.g Person Name Age
 -- e.g
 -- "Shape can be wither Circle (with float) OR Rectangle (Float and Float)"
 data Shape = Circle Float | Rectangle Float Float -- the pipe means 'OR'
+ deriving (Show) -- This tells the compiler how to represent the data
+ 		 -- __str__ like in python for Objects
 
+-- NOTE: Functions start always in lowercase...
 
+-- Each value represents a constructor
+data UnderGrad = CompSci | Management | Media
+ deriving (Show)
 
+-- Exmaple Constructor with value
+data Master = CyberSec Int | Business Int | Law Int
+ deriving (Show)
--- ws_05.hs
+++ ws_05.hs
@@ -12,3 +12,33 @@
 maybeDefault :: a -> Maybe a -> a -- Type definiton for the function
 maybeDefault x (Just y) = y -- Case where the 'Maybe container' Contains a valid y
 maybeDefault x Nothing = x -- Case where the "Maybe Container" contains 'Nothing/Null' as y value (Print default value)
+
+data Cmp = LESSER | EQUAL | GREATER -- data (struct like) 'lesser OR equal OR greater'
+ deriving (Show)
+
+compareOrd :: Ord a => a -> a -> Cmp -- Type dfinition (NOTE: last val is Cmp)
+compareOrd x y
+ | x < y = LESSER
+ | x > y = GREATER
+ | otherwise = EQUAL -- case where x == y
+
+-- TASK 2 --
+data UnderGrad = Accounting | Management | CompSci | Media -- Different Constructors
+ deriving (Show)
+
+data Master = DataScience | CyberSec | Business
+ deriving (Show)
+
+newtype Color = Color (Int, Int, Int) -- 1 Constructor 'Color' with val (...)
+ deriving (Show)
+-- Now, creation of the smart constructor
+-- Smart Constructor is literally a function to create the instance
+-- of the 'struct'/newtype/data based on logic
+mkColor :: (Int, Int, Int) -> Maybe Color -- Return a 'Color' if everything goes fine
+mkColor (r, g, b)
+ | r >= 0 && r <= 255 &&
+   g >= 0 && g <= 255 &&
+   b >= 0 && b <= 255 = Just (Color (r, g, b))
+ |otherwise = Nothing -- None/null if x not in range
+
+-- TASK 2. 3 ....
