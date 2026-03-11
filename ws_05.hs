c31163313fcec22f8cee9016b6b748fa9999870b
ignant <ignanttt@gmail.com>
2026-03-11 22:38:30 +0100

    UPDATED | w5 ws

--- ws_05.hs
+++ ws_05.hs
@@ -41,4 +41,61 @@ mkColor (r, g, b)
    b >= 0 && b <= 255 = Just (Color (r, g, b))
  |otherwise = Nothing -- None/null if x not in range
 
--- TASK 2. 3 ....
+data Person = Student String Int Int String
+             | AcademicStaff String Int String
+	     | ProfServices String Int String
+	     | ContStaff String Float
+ deriving (Show)
+
+isStudent :: Person -> Bool -- Type definition, the function expects a 'type Person' as input
+isStudent (Student _ _ _ _) = True
+isStudent _ = False
+
+personName :: Person -> String -- Type definition, the function expects a 'type Person' as input
+personName (Student name _ _ _) = name
+personName (AcademicStaff name _ _) = name
+personName (ProfServices name _ _) = name
+personName (ContStaff name _) = name
+personName _ = "No Name"
+
+-- data + smart constructor
+data Student = CS | SE | Cyber | DS | Other
+ deriving (Show) -- __str__ like in py
+
+data Color2 = Blue (Int, Int, Int)
+            | Red (Int, Int, Int) 
+	    | Green (Int, Int, Int) 
+	    | Purple (Int, Int, Int) 
+	    | Gray (Int, Int, Int)
+ deriving (Show) -- __str__ like in py
+
+makeStudent :: Student -> Color2 -- Type definition, input of 'type Student' and output 'type Color2'
+
+-- Logic flow using pattern matching
+makeStudent CS = Blue (0, 0, 2055)
+makeStudent SE = Red (255, 0, 0)
+makeStudent Cyber = Green (0, 255, 0)
+makeStudent DS = Purple (128, 0, 128)
+makeStudent Other = Gray (200, 200, 200)
+
+-- Database system for Uni
+-- So I'll create two custom datatypes, a newtype for the database, and a data for each record
+
+newtype Database = Database [StuRecord] -- Contructor DB builds a list containig a StuRecord type (tuple of int)
+
+-- Smart Constructor for range of 0 - 100 for the grade
+newtype StuRecord = Record (Int, Int) -- Where first Int is ID, and second Int is avrg grade (0 - 100)
+ deriving (Show) -- for __str__ like in py
+
+makeRecord :: (Int, Int) -> Maybe StuRecord
+makeRecord (x, y)
+ | y >= 0 && y <= 100 = Just (Record (x, y)) -- NOTE: Maybe always comes with 'Just()'
+                                             -- And the value need to match the name of the CONSTRUCTOR,
+					     -- not the name of the 'type value'
+ | otherwise = Nothing -- Return null/None value
+
+-- Helper function to add a record to the database
+addRecord :: StuRecord -> Database -> Database
+addRecord rec (Database records) = Database(rec:records) -- Append record to the db
+
+
