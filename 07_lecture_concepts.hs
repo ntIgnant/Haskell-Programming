5375fb509548c8dd6306aeb432eb67951e3d19bc
ignant <ignanttt@gmail.com>
2026-04-09 22:09:40 +0200

    UPDATED | w07 foldl, local bindings

--- 07_lecture_concepts.hs
+++ 07_lecture_concepts.hs
@@ -23,3 +23,36 @@ lengthList xs = foldl (\acc x -> acc + 1) 0 xs
 
 allTrue :: [Bool] -> Bool -- Type definition | Return True only if all the elements of the list are True
 allTrue xs = foldl (\val x -> val && x) True xs
+
+lenList :: [a] -> Int -- Type definition | get a list of any type of element and return the length
+	  -- foldl (function) (base value) (list) | NOTE: the funcion is actually an ACCUMULATOR
+lenList xs = foldl (\acc x -> acc + 1) 0 xs -- \acc .... is a lamnda function, it is an anonymous funct.
+
+sumEven :: [Int] -> Int -- Type definition | sum all the Even numbers of a list
+	  -- foldl (function/accumulator) (base value) (list)
+sumEven xs = foldl (\acc x -> if x `mod` 2 == 0 then acc + x else acc) 0 xs
+
+
+-- Local Bindings (local variables)
+-- For local variable sin Haskell, ther is 'where' and 'let ... in'
+dummy1 x = b + 3
+ where
+  a = x + 1
+  b = a * 2
+
+distance :: Floating a => a -> a -> a -- Type definition | get the distance between two points
+distance x y = sqrt (x2 + y2)
+ where
+  x2 = x^2
+  y2 = y^2
+
+circleArea :: Float -> Float -- Type definition | using local bindings 'let...in'
+circleArea x =
+ let x2 = x^2
+ in pi * x2
+
+distanceLet :: Floating a => a -> a -> a -- Type definition | using let...in as local bindings
+distanceLet x y =
+ let x2 = x^2
+     y2 = y^2
+ in sqrt(x2 + y2)
