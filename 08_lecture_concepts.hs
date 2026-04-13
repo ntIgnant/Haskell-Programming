e6bc963bd11159e9f3d8fe3c85fcee4fe004527b
ignant <ignanttt@gmail.com>
2026-04-13 23:44:59 +0200

    UPDATED | w8 do notation, and main function

--- 08_lecture_concepts.hs
+++ 08_lecture_concepts.hs
@@ -6,5 +6,10 @@
 
 -- Pure functions take normal values and return normal values, with no effects.
 
-
-
+-- Example of IO Action and do Notation (basic main function)
+main :: IO() -- IO wrapper for datatype definition (handle datatype IO errors)
+main = do
+ putStrLn "Type your Name: "
+ -- NOTE: '<-' is only allowed inside 'do notation'
+ name <- getLine -- get input line (user name) and assign it to 'name' variable
+ putStrLn ("Hey, " ++ name ++ " what's Up")
