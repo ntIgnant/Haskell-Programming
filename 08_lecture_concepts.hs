dda1f7f392a7a11692d5dbcc94c35f1bf4f4be23
ignant <ignanttt@gmail.com>
2026-04-15 23:30:28 +0200

    UPDATED | w8 IO actions, do notation, and returns

--- 08_lecture_concepts.hs
+++ 08_lecture_concepts.hs
@@ -6,6 +6,7 @@
 
 -- Pure functions take normal values and return normal values, with no effects.
 
+{-
 -- Example of IO Action and do Notation (basic main function)
 main :: IO() -- IO wrapper for datatype definition (handle datatype IO errors)
 main = do
@@ -13,3 +14,13 @@ main = do
  -- NOTE: '<-' is only allowed inside 'do notation'
  name <- getLine -- get input line (user name) and assign it to 'name' variable
  putStrLn ("Hey, " ++ name ++ " what's Up")
+-}
+
+-- So for functions that contain IO() 'IO Actions' a 'return' or 'pure' is supposed to exis
+-- to 'turn' the datatype into IO type. A function that gets IO, should always return IO type
+
+-- So all the functions with ... :: IO()
+
+
+
+
