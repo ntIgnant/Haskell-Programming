2d5db409630c901a224bb3595bfba7040a22148d
ignant <ignanttt@gmail.com>
2026-05-02 11:03:07 +0200

    updated: w9 concepts

--- 09_lecture_concepts.hs
+++ 09_lecture_concepts.hs
@@ -1,2 +1,22 @@
+import Data.Char (toUpper)
+import Data.List
+
 -- Haskell Libraries
+-- Major top-level categories include:
+-- 	Control: control flow related functions
+-- 	Data: e.g types, trees, maps, graphs
+-- 	Foreign: Anything to do with interaction with other languages
+-- 	GHC: Modules for the compiler
+-- 	System: Modules to interact with the OS
+-- 	Text: Alternative ways to represent text/strings associated functions
+
+
+-- E.g Data
+main :: IO()
+
+main = do
+ let myName = "oscar"
+ putStrLn (map toUpper myName) -- use map because 'toUpper' applies only to char (no whole string)
+
+
 -- Exceptions
